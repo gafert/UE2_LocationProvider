c9ca6c6d7c32c9e289a052e1c4767333a74e4f0a
gafert <m.gafert@outlook.com>
2017-04-24 16:32:13 +0200

    Setup

---
+++ app/src/main/java/fhtw/bsa2/gafert_steiner/ue2_locationprovider/LocationActivity.java
@@ -0,0 +1,13 @@
+package fhtw.bsa2.gafert_steiner.ue2_locationprovider;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LocationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_location);
+    }
+}
