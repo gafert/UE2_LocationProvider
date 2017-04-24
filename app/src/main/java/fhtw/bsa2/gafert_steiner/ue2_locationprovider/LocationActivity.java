acf915257c74b520ced97cf092e1b894ac56dedb
gafert <m.gafert@outlook.com>
2017-04-24 21:56:52 +0200

    Added Location MockService and basic ListView with Adapter

--- app/src/main/java/fhtw/bsa2/gafert_steiner/ue2_locationprovider/LocationActivity.java
+++ app/src/main/java/fhtw/bsa2/gafert_steiner/ue2_locationprovider/LocationActivity.java
@@ -2,6 +2,13 @@ package fhtw.bsa2.gafert_steiner.ue2_locationprovider;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+import fhtw.bsa2.hammer.mocklocationprovider.StartMockservice;
 
 public class LocationActivity extends AppCompatActivity {
 
@@ -9,5 +16,25 @@ public class LocationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location);
+
+        // New Mockservice from Library
+        StartMockservice mockService = new StartMockservice(this, getResources().openRawResource(R.raw.route_example));
+
+        mockService.setShowLog(true);
+        mockService.start();
+
+        // Adapter with own defined layout
+        // Alternatively (standard list item): R.layout.simple_list_item_1
+        // Alternatively (static source): ArrayAdapter movieAdapter = new
+        // ArrayAdapter<String> (this, R.layout.activity_list_view, nom_array);
+        ArrayAdapter listAdapter = new ArrayAdapter<String>(this, R.layout.list_element, new ArrayList<String>());
+
+        listAdapter.add("1st Entry, made by LocationActivity");
+        listAdapter.add("2nd Entry also made by LicationActivity");
+
+        // ListView
+        ListView coordinateListView = (ListView) findViewById(R.id.coordinate_list_view);
+        // Relate Adapter and ListView
+        coordinateListView.setAdapter(listAdapter);
     }
 }
